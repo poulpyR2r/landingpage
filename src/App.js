f40940d06c750b795c50ea6a20855f9168b83e62
poulpyR2r <guewencarre@gmail.com>
2023-12-06 19:47:16 +0100

    deployv3

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import React, { useEffect } from "react";
 // import google from "./images/android.png";
 // import appstore from "./images/apple.png";
 import map from "./images/map.png";
+import interactiveMap from "./images/interractive.png";
+import certfied from "./images/certfied.png";
+import guide from "./images/guide.png";
+import ecomaplogo from "./images/ecomaplogo.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
 import { useForm, ValidationError } from "@formspree/react";
 
 function App() {
   const [state, handleSubmit] = useForm("mleyqdyv");
 
   const [succesForm, setSuccesForm] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   useEffect(() => {
     if (state.succeeded) {
@@ -19,49 +26,80 @@ function App() {
 
   return (
     <div className="App">
-      <nav className="Container1">
-        <div class=" container mx-auto px-6 py-2 flex justify-between items-center w-full">
-          <a class="font-bold text-2xl lg:text-4xl" href="#">
-            Eco-Map
-          </a>
-          <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none">
-              <svg
-                class="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>XXXXXX</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
-          <div class="hidden lg:block">
-            <ul class="inline-flex">
-              <li>
-                <a class="px-4 font-bold" href="/">
-                  Product
-                </a>
-              </li>
+      <div className="container mx-auto ">
+        <header className="sm:px-8 px-2 py-2 z-10 ">
+          <nav className="flex justify-between items-center max-container">
+            <a href="/" className="">
+              <img className="logo" src={ecomaplogo}></img>
+            </a>
+            <ul className=" flex justify-center items-center gap-16 max-lg:hidden">
               <li>
                 <a class="px-4 font-bold" href="/">
                   About us
                 </a>
               </li>
               <li>
-                <a class="px-4 hover:text-gray-800" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a class="px-4 hover:text-gray-800" href="#">
+                <a
+                  class="px-4 hover:text-gray-800"
+                  href="#form"
+                  onClick={() => {
+                    setIsMenuOpen(!isMenuOpen);
+                  }}
+                >
                   Contact us
                 </a>
               </li>
             </ul>
+
+            <div
+              className="hidden max-lg:block cursor-pointer"
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}
+            >
+              <RxHamburgerMenu className="text-4xl" />
+            </div>
+          </nav>
+        </header>
+        {isMenuOpen && (
+          <div>
+            <nav className="fixed top-0 right-0 left-0 bottom-0 lg:bottom-auto bg-slate-100  ">
+              <div
+                className="hidden max-lg:block fixed right-0  px-8 py-4 cursor-pointer"
+                onClick={() => {
+                  setIsMenuOpen(!isMenuOpen);
+                }}
+              >
+                <AiOutlineClose className="text-4xl" />
+              </div>
+              <ul className=" lg:hidden flex flex-col items-center justify-center h-full ">
+                <li>
+                  <a
+                    class="px-4 font-bold"
+                    href="/"
+                    onClick={() => {
+                      setIsMenuOpen(!isMenuOpen);
+                    }}
+                  >
+                    About us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="px-4 hover:text-gray-800"
+                    href="#form"
+                    onClick={() => {
+                      setIsMenuOpen(!isMenuOpen);
+                    }}
+                  >
+                    Contact us
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
-        </div>
-      </nav>
+        )}
+      </div>
 
       {/* hero  */}
 
@@ -75,68 +113,55 @@ function App() {
             <img className="imageMap" src={map}></img>
           </div>
         </div>
-        <div className="flex justify-center items-center mt-6">
-          <button className="Button">Explore</button>
+        <div className="flex justify-center items-center mt-0">
+          <a href="#section">
+            <button className="Button animate-bounce"> Découvrire</button>
+          </a>
         </div>
       </div>
 
       {/* SECTION */}
-      <div className="Container2 py-20">
-        <div className="container mx-auto px-6  flex justify-center">
+      <div className="container py-20 mx-auto" id="section">
+        <div className="px-6 mb-28">
+          <h1 className="titlePropose">Ce qu’on vous propose</h1>
+        </div>
+        <div className="px-6 flex justify-center">
           <div>
-            <div class="grid grid-cols-3 gap-48">
-              <div className="Card">
-                <div className="titleCard flex flex-col justify-center pl-9 pr-16 py-6 rounded-[32px_32px_0px_0px] items-start max-md:px-2">
-                  <div className="justify-center items-center flex  max-w-full">
-                    <img
-                      loading="lazy"
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/518f68182f7f7d4614e4bb585c6470980f2bc914cde17383d7fbd19b99232022?"
-                      className="aspect-square object-contain object-center w-[60px] overflow-hidden shrink-0 flex-1 my-auto mr-4"
-                    />
-                    <div className="text-gray-800 text-2xl font-bold self-stretch grow whitespace-nowrap">
-                      7
-                    </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  className="w-full"
+                  src={interactiveMap}
+                  alt="Sunset in the mountains"
+                />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">
+                    Carte interactive !
                   </div>
-                </div>
-                <div className="bodyCard">
-                  Une carte interactive pour localiser des emplacements dédiés à
-                  des causes engagées.
+                  <p className="text-gray-700 text-base">
+                    Une carte interactive pour localiser des emplacements dédiés
+                    à des causes engagées.
+                  </p>
                 </div>
               </div>
-              <div className="Card">
-                <div className="titleCard flex flex-col justify-center pl-9 pr-16 py-6 rounded-[32px_32px_0px_0px] items-start max-md:px-2">
-                  <div className="justify-center items-center flex   max-w-full">
-                    <img
-                      loading="lazy"
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/518f68182f7f7d4614e4bb585c6470980f2bc914cde17383d7fbd19b99232022?"
-                      className="aspect-square object-contain object-center w-[60px] overflow-hidden shrink-0 flex-1 my-auto mr-4"
-                    />
-                    <div className="text-gray-800 text-2xl font-bold self-stretch grow whitespace-nowrap">
-                      80%
-                    </div>
-                  </div>
-                </div>
-                <div className="bodyCard">
-                  Recherchez et identifiez des emplacements à certifier en
-                  utilisant des filtres.
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={certfied} alt="certifié" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">Certifié !</div>
+                  <p className="text-gray-700 text-base">
+                    Recherchez et identifiez des emplacements certifié en
+                    utilisant des filtres.
+                  </p>
                 </div>
               </div>
-              <div className="Card">
-                <div className="titleCard flex flex-col justify-center pl-9 pr-16 py-6 rounded-[32px_32px_0px_0px] items-start max-md:px-2">
-                  <div className="justify-center items-center flex  max-w-full">
-                    <img
-                      loading="lazy"
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/518f68182f7f7d4614e4bb585c6470980f2bc914cde17383d7fbd19b99232022?"
-                      className="aspect-square object-contain object-center w-[60px] overflow-hidden shrink-0 flex-1 my-auto mr-4"
-                    />
-                    <div className="text-gray-800 text-2xl font-bold self-stretch grow whitespace-nowrap">
-                      58%
-                    </div>
-                  </div>
-                </div>
-                <div className="bodyCard">
-                  Un manuel détaillé fournissant des explications complètes sur
-                  les différents labels.
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={guide} alt="Guide" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">Explication !</div>
+                  <p className="text-gray-700 text-base">
+                    Un manuel détaillé fournissant des explications complètes
+                    sur les différents labels.
+                  </p>
                 </div>
               </div>
             </div>
@@ -144,7 +169,10 @@ function App() {
         </div>
       </div>
 
-      <div className="container mx-auto px-6 py-2 flex flex-col  w-full mb-28">
+      <div
+        className="container mx-auto px-6 py-2 flex flex-col  w-full mb-28"
+        id="form"
+      >
         <h1 className="titleCallToAction">Vous voulez en savoir plus ?</h1>
         <p className="textCallToAction">
           Saisissez votre adresse électronique pour obtenir plus d'informations
